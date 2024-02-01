81e0f48a46d82c890b97d4e1131d432998824522
Gaurav Chaudhary <156519357+Gaurav-Chaudhary-01@users.noreply.github.com>
2024-02-01 21:52:16 +0530

    Add files via upload

---
+++ backend4/config.js
@@ -0,0 +1,5 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://gauravchaudhary97g:chaudhary7454@sellerkin-project.j2yxhvy.mongodb.net/words-collection?retryWrites=true&w=majority';
+
